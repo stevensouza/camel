b835c1803369a4f3c45da7b7d7ba28cdeb612d3e
Steve Souza <steve@stevesouza.com>
2018-12-12 15:19:48 -0500

    autoconvert to/from json and xml to pojo.  also made change to now use existing tomcat spring boot server in rest calls and not spin up a seperate jetty server.

--- experiment3_rest/src/main/java/com/stevesouza/camel/experiment2/MyCamelRoutes.java
+++ experiment3_rest/src/main/java/com/stevesouza/camel/experiment2/MyCamelRoutes.java
@@ -1,9 +1,9 @@
 package com.stevesouza.camel.experiment2;
 
 import org.apache.camel.component.metrics.routepolicy.MetricsRoutePolicyFactory;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,10 +18,6 @@ public class MyCamelRoutes extends SpringRouteBuilder {
      */
 
 
-    @Value("${camel.route.http.port}")
-    private int port;
-
-
     // @formatter:off - disable intellij's reformat command from messing up indentation in camel routes
     /** Defined routes:
 
@@ -32,17 +28,33 @@ public class MyCamelRoutes extends SpringRouteBuilder {
         // see hawt io 'Route Metrics' for jamon like data it collects.
         getContext().addRoutePolicyFactory(new MetricsRoutePolicyFactory());
 
-        from("jetty://http://0.0.0.0:{{camel.route.http.port}}/hello")
+//  If you use camel-jetty-starter can do this however then it loads both tomcat and jetty, so using 'servlet'
+// component just reuses tomcat
+//        from("jetty://http://0.0.0.0:{{camel.route.http.port}}/hello")
+//                .routeId("route.helloWorld")
+//                .transform().simple("hello world");
+
+//      Could configure like the below, but instead I did it in application.properties
+//       restConfiguration()
+//                .contextPath("rest"). // uses '/camel' by default
+// the following are set in application properties or default is taken.
+//                .port(port)
+//                .bindingMode(RestBindingMode.json_xml)
+//                .dataFormatProperty("prettyPrint", "true");
+
+        // using servlet component and not rest dsl. note could also use jetty as above
+        // will use localhost:8080/rest/hello - using default tomcat server
+        // default would be localhost:8080/camel/hello
+         from("servlet://hello")
                 .routeId("route.helloWorld")
                 .transform().simple("hello world");
 
-        restConfiguration()
-                .contextPath("rest")
-                .port(port);
 
         // returns person.toString();
-        rest().produces("text/plain")
-                .get("/random").to("bean:generateData?method=getRandomPerson");
+        rest()
+                .produces(MediaType.APPLICATION_JSON.toString())
+                .get("/random")
+                .to("bean:generateData?method=getRandomPerson");
 
     }
     // @formatter:on - enable intellij's reformat command after having disabled it for the above camel routes
--- experiment3_rest/src/main/java/com/stevesouza/camel/experiment2/data/Person.java
+++ experiment3_rest/src/main/java/com/stevesouza/camel/experiment2/data/Person.java
@@ -3,8 +3,12 @@ package com.stevesouza.camel.experiment2.data;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 @Data
 @NoArgsConstructor
+// the following is needed for jaxb to render as xml though jackson doesn't need this annotation for json
+@XmlRootElement
 public class Person {
     private PersonName personName;
     private String personDescriptionText;
