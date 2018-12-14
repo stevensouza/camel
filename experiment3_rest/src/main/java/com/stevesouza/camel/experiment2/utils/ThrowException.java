29b7d388be02ec9dfa9df5186eeaf724653ad832
Steve Souza <steve@stevesouza.com>
2018-12-14 11:57:08 -0500

    forgot to include file that allows for testing of exception handling in camel routes

---
+++ experiment3_rest/src/main/java/com/stevesouza/camel/experiment2/utils/ThrowException.java
@@ -0,0 +1,15 @@
+package com.stevesouza.camel.experiment2.utils;
+
+public class ThrowException {
+
+    public void throwException() throws Exception {
+        throw new CustomerValidation("Simulating a customer validation exception to show how camel maps exceptions to http status codes");
+
+    }
+
+    public class CustomerValidation extends Exception {
+        public CustomerValidation(String message) {
+            super(message);
+        }
+    }
+}
