c1307a139fadd1ee4d7396b784b90fe1a6ed43d1
Steve Souza <steve@stevesouza.com>
2019-03-08 13:54:36 -0500

    simplified code and removed classes that were not relevant to calling a soap webservice

--- experiment6_soap/src/main/java/com/stevesouza/camel/experiment6/MyCamelRoutes.java
+++ experiment6_soap/src/main/java/com/stevesouza/camel/experiment6/MyCamelRoutes.java
@@ -1,9 +1,6 @@
 package com.stevesouza.camel.experiment6;
 
-//import com.stevesouza.camel.experiment6.data.Person;
-//import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.component.metrics.routepolicy.MetricsRoutePolicyFactory;
-//import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,120 +8,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyCamelRoutes extends SpringRouteBuilder {
 
-    /**
-     * camel kafka component documentaiton:
-     * https://github.com/apache/camel/blob/master/components/camel-kafka/src/main/docs/kafka-component.adoc
-     * <p>
-     * to view hawt io (camel routes, jmx) - http://localhost:8080/actuator/hawtio/
-     * activemq - http://localhost:8161/
-     */
-
-    // Route from/to information has been externalized into application.properties
-    @Value("${camel.route.generateRandomData.from}")
-    private String generateRandomDataFrom;
-
-    @Value("${camel.route.readFromKafka.from}")
-    private String readFromKafkaFrom;
-
-    @Value("${camel.route.processData.kafka.to}")
-    private String processDataKafkaTo;
-
-    @Value("${camel.route.processData.activemq.to}")
-    private String processDataActiveMqTo;
-
+    // externalized information for accessing math web service
+    @Value("${camel.route.math.from}")
+    private String readFromWebService;
 
     // @formatter:off - disable intellij's reformat command from messing up indentation in camel routes
     /** Defined routes:
-     *      - route.generateRandomData.controlbus - Called from rest controller to start/stop the main route
-     *          (route.generateRandomData)
-     *      - route.generateRandomData - generate person data and write to amq/kafka
+     *      - route.math" - Called from rest controller to perform math by calling cxf soap webservices
+     *          component which in turn calls a publicly available rest endpoint that either adds, subtracts,
+     *          multiplies or divides 2 integers
+     *      - to view hawt io (camel routes, jmx) - http://localhost:8080/actuator/hawtio/
      *
      */
     @Override
     public void configure() {
         // enable dropwizard metrics - not required
         // see hawt io 'Route Metrics' for jamon like data it collects.
+        //
         getContext().addRoutePolicyFactory(new MetricsRoutePolicyFactory());
 
-        from("direct:delme")
-                .routeId("route.delme")
-    .setBody(constant("12345"))
-    .bean(AddBuilder.class)
-
-    .setHeader("operationName", constant("Add"))
-
-    .to("cxf://http://www.dneonline.com/calculator.asmx"
-        + "?serviceClass=org.tempuri.CalculatorSoap"
-        + "&wsdlURL=service.wsdl")
-    .log("The response was ${body[0]}")
-        .transform(simple("The response was ${body[0]}"));
-
-        // Control main route
-        from("direct:delmex")
-                .routeId("route.delmex")
-                .log("header.action=${header.action}")
-                .transform(simple("Action taken on route: ${header.action}"));
-//        from("direct:generateRandomData.controlbus")
-//               .routeId("route.generateRandomData.controlbus")
-//               .log("action=${header.action}")
-//               // controlbus is an eip that allows you to start/stop/suspend/resume routes among other things.
-//               .toD("controlbus:route?routeId=route.generateRandomData&action=${header.action}")
-//               .transform(simple("Action taken on route: ${header.action}"));
-
-        // Generate random Person data
-        // could use timer or quartz instead of schedule component
-//        from(generateRandomDataFrom)
-//                .routeId("route.generateRandomData")
-//                .noAutoStartup() // don't run route on app startup. The control bus below will start/stop it on demand
-//                // The methods return value will be used to setBody(..)
-//                .bean(GenerateData.class)
-//                .log("Generated random data=${body}")
-//                .marshal().json(JsonLibrary.Jackson)
-//                .to("direct:processData");
-
-        // Send data on to either amq or kafka depending on configuration
-        // always assign a routeId for easy route identification
-        //.noAutoStartup()
-        // You can set a node description with: .description("my description")
-//        from("direct:processData")
-//              .routeId("route.processData")// set the route name and description which will display in hawtio
-//              .routeDescription("Route that either sends message to kafka, or amq based on config property")
-//              .log("Processing data=${body}")
-//                .choice()
-//                    .id("Choice: kafka or amq?")// the name for this 'choice' node which will show up in hawtio route diagram
-//                    // getting property for spring application.properties file
-//                    .when(simple("${properties:experiment6.broker_type} == 'kafka'"))
-//                        .log("broker_type is kafka")
-//                        .setHeader(KafkaConstants.KEY, constant("person json")) // Key of the message
-//                        .toD(processDataKafkaTo)
-//                    .otherwise()
-//                        .log("broker type is activemq")
-//                        //  '#jms' is used to look up the spring bean of that name to get the connectionFactory from
-//                        .toD(processDataActiveMqTo)
-//                .end();
-
-
-
-        // note i use the activemq component.  it inherits from the jms component and uses the same properties
-        // however it is more efficient than jms.
-        // read from activemq
-        // showing alternative way of defining from clause in a property
-//         from(simple("{{camel.route.readFromActiveMq.from}}").getText())
-//                .routeId("route.readFromActiveMq")
-//                // note if i didn't unmarshal json text would print instead of the toString method
-//                // also i believe you can register a MessageConverter that puts the class name in a message
-//                // header and you don't need to specify the class explicitly. You wouldn't need to unmarshal
-//                // if the rest of the route was ok with dealing with json.
-//                .unmarshal().json(JsonLibrary.Jackson, Person.class)
-//                .log("Retrieved data from amq queue: ${body}");
-
-        // read data that was written to kafka
-        // note apache website is outdated.  This seems to be more accurate:
-        //   https://github.com/apache/camel/blob/master/components/camel-kafka/src/main/docs/kafka-component.adoc
-//        from(readFromKafkaFrom)
-//                .routeId("route.readFromKafka")
-//                .log("Retrieved data from kafka topic: ${body}");
-
+        from("direct:math")
+                .routeId("route.math")
+                .log("${header.operationName}: ${body}")
+                .toD(readFromWebService)
+                .setBody(simple("The ${header.operationName} response was ${body}"))
+                .log("${body}");
     }
     // @formatter:on - enable intellij's reformat command after having disabled it for the above camel routes
 
--- experiment6_soap/src/main/java/com/stevesouza/camel/experiment6/MySpringController.java
+++ experiment6_soap/src/main/java/com/stevesouza/camel/experiment6/MySpringController.java
@@ -4,38 +4,97 @@ package com.stevesouza.camel.experiment6;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.FluentProducerTemplate;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Note the following controller accepts 2 integer arguments and uses a camel
+ * web services route to call an external api to add, subtract, multiply, or
+ * divide the 2 numbers.  Most of the endpoints take a json array which is automatically
+ * converted to a List.  The cxf web service takes a List if the web service
+ * endpoint takes mutliple endpoints.  Note if the endpoint can take a specific
+ * object I suspect that could be passed directly (i.e. if the Add object could be submitted
+ * directly.
+ *
+ * The endpoints that have postmapping take a json payload of an array (unnamed) with 2 values.  Exactly
+ * like this: [5,6]. In the case of add being called the answer would be 11, and in the case of multiply
+ * the answer would be 30 etc.
+ *
+ * Here are the endpoints that would receive the above mentioned array json
+ *  http://localhost:8080/math/add
+ *  http://localhost:8080/math/subtract
+ *  http://localhost:8080/math/multiply
+ *  http://localhost:8080/math/divide
+ *
+ * The following endpoint is the only one defined for a GET request:
+ *   http://localhost:8080/math/addwithparams?a=4&b=5
+ *
+ *   See https://github.com/apache/camel/blob/master/components/camel-cxf/src/main/docs/cxf-component.adoc
+ *   for more info on the camel cxf endpoint.  Note alternatively the spring webservices component
+ *   could be used https://github.com/apache/camel/blob/master/components/camel-spring-ws/src/main/docs/spring-ws-component.adoc
+ */
 
 @RestController
-@RequestMapping(value = "/experiment6")
+@RequestMapping(value = "/math")
 @Slf4j
 public class MySpringController {
-    @Value("${experiment1.broker_type}")
-    private String brokerType;
 
     // note you could reuse the same FluentProducerTemplate and set the endpoint (probably template.to("direct:start")
-    @EndpointInject(uri = "direct:delme")
-    FluentProducerTemplate controlBus;
+    @EndpointInject(uri = "direct:math")
+    private FluentProducerTemplate math;
 
-    @GetMapping("/start")
-    public String start() {
-        // request() expects a return falue and send() does not.
-        // action could be either start or resume
-        log.info("starting/resuming route");
-        // note in this case the route returns info to the browser, though only doing it this way ot show how it is done.
-        // probably best to just do a plain 'send' and have the controller itself return the message.
-        return controlBus.withHeader("action", "resume").request(String.class);
+    @PostMapping("/add")
+    public String add(@RequestBody List<Integer> list) {
+        return callSoapMathService(list, "Add");
     }
 
-    @GetMapping("/stop")
-    public String stop() {
-        // action could either be stop or suspend
-        log.info("stopping/suspending route");
-        return controlBus.withHeader("action", "suspend").request(String.class);
+    @PostMapping("/subtract")
+    public String subtract(@RequestBody List<Integer> list) {
+        return callSoapMathService(list, "Subtract");
     }
+
+    @PostMapping("/multiply")
+    public String multiply(@RequestBody List<Integer> list) {
+        return callSoapMathService(list, "Multiply");
+    }
+
+    @PostMapping("/divide")
+    public String divide(@RequestBody List<Integer> list) {
+        return callSoapMathService(list, "Divide");
+    }
+
+    /**
+     * Adds integers a+b and returns the reponse
+     *
+     * example: http://localhost:8080/experiment6/addwithlist?a=13&b=17
+     *
+     * would return: response is 30
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    @GetMapping("/addwithparams")
+    public String addWithList(@RequestParam("a") int a, @RequestParam("b") int b) {
+        return callSoapMathService(build(a, b), "Add");
+    }
+
+    private String callSoapMathService(@RequestBody List<Integer> list, String operationName) {
+        return math
+                .withHeader("operationName", operationName)
+                .withBody(list)
+                .request(String.class);
+    }
+
+
+    private List<Integer> build(int a, int b) {
+        List<Integer> list = new ArrayList<>();
+        list.add(a);
+        list.add(b);
+        return list;
+    }
+
 
 }
